79abffc23072535b1aadcdf4f8cc8819014e6831
Ricardo <cesar15bernardo@gmail.com>
2025-07-16 00:06:35 +0100

    Organizei o cod

--- frontend/src/pages/home/App.tsx
+++ frontend/src/pages/home/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
 
       <main>
+        
         <div className="conteiner">
           <div className='titulo'>
             <h1>ATM CHECK</h1>
@@ -20,19 +21,20 @@ function App() {
             <a className='btn-session-agent' href="#">Agent</a>
           </div>
 
-
-
-          <p>Explore nossa Plataforma <strong>ATM Express</strong> e descubra os <strong>melhores </strong>pontos de Caixa Eletronico.</p>
+          <p>Explore nossa Plataforma <strong>ATM CHECK</strong> e descubra os <strong>melhores </strong>pontos de Caixa Eletronico.</p>
 
           <div className="link-client">
             <a href="#">Check ATM</a>
           </div>
 
         </div>
+
       </main>
+
       <footer>
         <p>&copy; 2025 Todos direitos reservados.</p>
       </footer>
+
     </div>
   );
 }
