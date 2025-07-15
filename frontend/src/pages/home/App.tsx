ad611302de57cd2c33e9b6cbb63e31d7ba7230b8
Ricardo <cesar15bernardo@gmail.com>
2025-07-15 20:25:44 +0100

    Add one more link

--- frontend/src/pages/home/App.tsx
+++ frontend/src/pages/home/App.tsx
@@ -20,11 +20,19 @@ function App() {
             <a className='btn-session-agent' href="#">Agent</a>
           </div>
 
-          <p>Explore nossa Plataforma <strong>ATM Express</strong> e descubra os <strong>melhores pontos</strong> de Caixa Eletronico.</p>
+
+
+          <p>Explore nossa Plataforma <strong>ATM Express</strong> e descubra os <strong>melhores </strong>pontos de Caixa Eletronico.</p>
+
+          <div className="link-client">
+            <a href="#">Check ATM</a>
+          </div>
 
         </div>
       </main>
-
+      <footer>
+        <p>&copy; 2025 Todos direitos reservados.</p>
+      </footer>
     </div>
   );
 }
