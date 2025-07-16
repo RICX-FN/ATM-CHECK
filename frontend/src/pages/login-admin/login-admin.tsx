3656a02e6b78c0e703a4e5ec7a859f6a7e13e7b1
Nelson Dombaxi <nelsondombaxi6@gmail.com>
2025-07-16 21:12:45 +0100

    alterações na tela de login-adm

--- frontend/src/pages/login-admin/login-admin.tsx
+++ frontend/src/pages/login-admin/login-admin.tsx
@@ -4,24 +4,27 @@ import Button from '../../components/button/Button';
 import EmailInput from '../../components/email-input/EmailInput'
 
 function LoginAdmin() {
-    const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleSubmit = (e: React.FormEvent) => {
-      e.preventDefault();
-      alert(`E-mail enviado: ${email}`);
-    };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert(`E-mail enviado: ${email}`);
+  };
 
-    return (
-      <div className="Login-Admin">
+  return (
+    <div className="Login-Admin">
+      <div className="login-box">
+        <h1> ATM CHECK</h1>
+        <h2>Admistrador faça o seu login</h2>
 
         <form onSubmit={handleSubmit}>
           <EmailInput value={email} onChange={(e) => setEmail(e.target.value)} required />
-            
-          <Button label="Clique Aqui" />
+          <EmailInput value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <Button label="Entrar" />
         </form>
-
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default LoginAdmin;
+export default LoginAdmin;
