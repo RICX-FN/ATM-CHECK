e6bf252c34ce2a135f61710a9e4a6c17b53ad57f
Nelson Dombaxi <nelsondombaxi6@gmail.com>
2025-08-08 18:29:36 +0100

    fix- botão de logout funciona

--- frontend/src/components/btn-logout/BtnLogout.tsx
+++ frontend/src/components/btn-logout/BtnLogout.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import '../btn-logout/btn-logout.css';
-import { ReactElement } from 'react';
 import { HiOutlineLogout } from 'react-icons/hi';
-
+import { useNavigate } from 'react-router-dom';
+import { ReactElement } from 'react';
 
 interface BtnLogoutProps {
-
-    onClick?: () => void;
-    type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
 }
 
 const BtnLogout: React.FC<BtnLogoutProps> = ({ onClick, type = "button" }) => {
-    return (
-        <button
-            type={type}
-            onClick={onClick}
-            className="btn-logout"
-        >
-            
-            {(HiOutlineLogout as any )({ size: 25, style: { color: '#fff', transition: '.7s ease;'} }) as ReactElement }
-            <span>Logout</span>
-        </button>
-    );
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+
+    if (onClick) onClick();
+
+    // Redireciona para a página de login (pode trocar para /login-agent se quiser)
+    navigate('/login-admin');
+  };
+
+  return (
+    <button
+      type={type}
+      onClick={handleLogout}
+      className="btn-logout"
+    >
+      {
+        (HiOutlineLogout as any)({
+          size: 25,
+          style: { color: '#fff', transition: '.7s ease' }
+        }) as ReactElement
+      }
+      <span>Logout</span>
+    </button>
+  );
 };
 
 export default BtnLogout;
