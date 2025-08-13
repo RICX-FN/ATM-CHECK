58cdd527e453d09ba18881d2efd4caaad2050c87
Nelson Dombaxi <nelsondombaxi6@gmail.com>
2025-08-13 21:51:49 +0100

    fix: resolvi o problema ddo botão do menu e tbm adicionei o icon de edit

--- frontend/src/pages/agent/Agent.tsx
+++ frontend/src/pages/agent/Agent.tsx
@@ -1,8 +1,11 @@
 // src/pages/dashboard/Dashboard.tsx
-
+import { MdOutlineEdit, MdMenu } from "react-icons/md";
+import { FiEdit } from "react-icons/fi";
+import { ReactElement } from "react";
 import React, { useState, useEffect } from 'react';
 import Sideagent from '../../components/agente-sidebar/Sideagent';
 import '../agent/agent.css';
+import SideBar from "../../components/sidebar/SideBar";
 
 function Agent() {
   const [userEmail, setUserEmail] = useState<string | null>(null);
@@ -20,6 +23,9 @@ function Agent() {
 
   return (
     <div className="Agent">
+      <button className="menu-toggle" onClick={toggleSidebar}>
+              {(MdMenu as any)({ size: 30 }) as ReactElement}
+            </button>
 
       {/* Agora passando activeTab para o SideBar */}
       <Sideagent
@@ -33,38 +39,43 @@ function Agent() {
         {activeTab === 'agent' && (
           <section className='session-agent'>
 
-              <div className="container-agent">
+            <div className="container-agent">
 
-                <p className='id-user-agent'>ID: user{}</p>
-                <h3 className='uni-box'>Unidades: {}</h3>
+              <p className='id-user-agent'>ID: user{ }</p>
+              <h3 className='uni-box'>Unidades: { }</h3>
 
-                <div className="box-card">
-                  <div className="card">
-                    <h1 className='value-sistem'>0</h1>
-                    <p className='description'>Descrição</p>
-                  </div>
-
-                  <div className="card">
-                    <h1 className='value-val'>0</h1>
-                    <p className='description'>Descrição</p>
-                  </div>
-
-                  <div className="card">
-                    <h1 className='value-papel'>0</h1>
-                    <p className='description'>Descrição</p>
-                  </div>
-
-                  <div className="card">
-                    <h1 className='value-lsc'>0</h1>
-                    <p className='description'>Descrição</p>
-                  </div>
+              <div className="box-card">
+                <div className="card">
+                  <h1 className='value-sistem'>0</h1>
+                  <p className='description'>Descrição</p>
                 </div>
 
+                <div className="card">
+                  <h1 className='value-val'>0</h1>
+                  <p className='description'>Descrição</p>
+                </div>
+
+                <div className="card">
+                  <h1 className='value-papel'>0</h1>
+                  <p className='description'>Descrição</p>
+                </div>
+
+                <div className="card">
+                  <h1 className='value-lsc'>0</h1>
+                  <p className='description'>Descrição</p>
+                </div>
+              </div>
+              
+              <div className="edit-icon">
+                {(FiEdit as any)({ size: 24, style: { cursor: 'pointer' } }) as ReactElement}
               </div>
 
-                <div className="field-btn">
-                  <button className='btn-aplicar'>Aplicar</button>
-                </div>
+            </div>
+
+
+            <div className="field-btn">
+              <button className='btn-aplicar'>Aplicar</button>
+            </div>
           </section>
         )}
 
