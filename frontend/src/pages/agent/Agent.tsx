4629b3dc64eaa1bd228d9b20e4e04166c85c7d11
Ricardo <cesar15bernardo@gmail.com>
2025-08-14 15:04:37 +0100

    Add: funcções dos campos de preechimento dos cards

--- frontend/src/pages/agent/Agent.tsx
+++ frontend/src/pages/agent/Agent.tsx
@@ -1,13 +1,14 @@
 // src/pages/dashboard/Dashboard.tsx
 import { MdOutlineEdit, MdMenu } from "react-icons/md";
 import { FiEdit } from "react-icons/fi";
-import { ReactElement } from "react";
+import { ReactElement, use } from "react";
 import React, { useState, useEffect } from 'react';
 import Sideagent from '../../components/agente-sidebar/Sideagent';
 import '../agent/agent.css';
-import SideBar from "../../components/sidebar/SideBar";
+import { set } from "react-hook-form";
 
 function Agent() {
+  const [showBtn, setShowBtn] = useState(false);
   const [userEmail, setUserEmail] = useState<string | null>(null);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [activeTab, setActiveTab] = useState<'agent' | 'routes' | 'notifications'>('agent');
@@ -21,11 +22,15 @@ function Agent() {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const handleShowBtnEdit = () => {
+    setShowBtn(!showBtn);
+  };
+
   return (
     <div className="Agent">
       <button className="menu-toggle" onClick={toggleSidebar}>
-              {(MdMenu as any)({ size: 30 }) as ReactElement}
-            </button>
+        {(MdMenu as any)({ size: 30 }) as ReactElement}
+      </button>
 
       {/* Agora passando activeTab para o SideBar */}
       <Sideagent
@@ -47,35 +52,61 @@ function Agent() {
               <div className="box-card">
                 <div className="card">
                   <h1 className='value-sistem'>0</h1>
-                  <p className='description'>Descrição</p>
+                  <p className='description'>Sistema</p>
                 </div>
 
                 <div className="card">
                   <h1 className='value-val'>0</h1>
-                  <p className='description'>Descrição</p>
+                  <p className='description'>Valores</p>
                 </div>
 
                 <div className="card">
                   <h1 className='value-papel'>0</h1>
-                  <p className='description'>Descrição</p>
+                  <p className='description'>Papel</p>
                 </div>
 
                 <div className="card">
                   <h1 className='value-lsc'>0</h1>
-                  <p className='description'>Descrição</p>
+                  <p className='description'>Levantamento sem cartão</p>
                 </div>
               </div>
-              
+
               <div className="edit-icon">
-                {(FiEdit as any)({ size: 24, style: { cursor: 'pointer' } }) as ReactElement}
+
+                <button className="btn-edit-field" onClick={handleShowBtnEdit}>
+                  {(FiEdit as any)({ size: 24, style: { cursor: 'pointer', color: 'white' } }) as ReactElement}
+                </button>
+              </div>
+              {/* campos de informação dos cards*/}
+              <div className="info-field" style={{ display: showBtn ? 'flex' : 'none' }}>
+                <p className="info-cards">Preencha o formulário</p>
+                <input className="field-info-card" placeholder="Sistema" type="text" />
+                <input className="field-info-card" placeholder="Valores" type="text" />
+
+                <div className="field-label-select">
+                  <label className="papel" htmlFor="papel-id">Papel</label>
+                  <label className="lsc" htmlFor="papel-id">L.Sem cartão</label>
+                </div>
+
+                <div className="field-select">
+                  <select className="field-papel" name="papel" id="papel-id">
+                    <option value="Com papel">Com papel</option>
+                    <option value="Sem papel">Sem papel</option>
+                  </select>
+
+                  <select className="field-lsc" name="papel" id="lsc-id">
+                    <option value="Com papel">Sem Levantamento</option>
+                    <option value="Sem papel">Com Levantamento</option>
+                  </select>
+                </div>
               </div>
 
             </div>
-
 
             <div className="field-btn">
               <button className='btn-aplicar'>Aplicar</button>
             </div>
+
           </section>
         )}
 
