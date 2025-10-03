3b64820702ac9cebb0f41bf92d2e24824f1c542c
Ricardo <cesar15bernardo@gmail.com>
2025-10-03 18:55:50 +0100

    UI cli

--- frontend/src/pages/cliente/Cliente.tsx
+++ frontend/src/pages/cliente/Cliente.tsx
@@ -8,7 +8,7 @@ function Cliente() {
         2. cards com os status dos ATMs
         */}
       <div className="container-cliente">
-        <h1 className="head-text-cliente">Explore</h1>
+        <h1 className="title-login" id="head-text-cliente">ATM CHECK</h1>
         <header className="header-cliente">
             <input className="search-bar-cliente" type="search" placeholder="Buscar por cidades"/>
         </header>
